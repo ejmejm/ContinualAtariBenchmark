0412d6ab654c3bc018f7ba50594ad9a167695dd5
ejmejm <ninjaedan@aol.com>
2025-02-11 11:38:12 -0600

    Changed env to be compatible with pretrained models

--- env.py
+++ env.py
@@ -5,12 +5,18 @@ import numpy as np
 
 
 class ContinualAtariEnv(gym.Env):
-    def __init__(self, game_order: List[str], steps_per_game: int, randomize_game_order: bool = False):
+    def __init__(
+        self,
+        game_order: List[str],
+        steps_per_game: int,
+        randomize_game_order: bool = False,
+    ):
         super().__init__()
         
         self.game_order = game_order
         self.steps_per_game = steps_per_game
         self.randomize_game_order = randomize_game_order
+        self.frameskip = 4 # Manually frameskip so that I have access to skipped frames
         self.current_game_idx = 0
         self.current_step = 0
         self.seed = None
@@ -18,12 +24,12 @@ class ContinualAtariEnv(gym.Env):
         self.action_space = gym.spaces.Discrete(18)
         self.observation_space = gym.spaces.Box(low=0, high=255, shape=(210, 160, 3), dtype=np.float32)
         
-        self.env = None
+        self.curr_game = None
         self.terminated = False
         
     def reset(self, seed: Union[int, None] = None) -> Tuple[np.ndarray, Dict]:
-        if self.env is not None:
-            self.env.close()
+        if self.curr_game is not None:
+            self.curr_game.close()
             
         self.current_game_idx = 0
         self.current_step = 0
@@ -35,35 +41,67 @@ class ContinualAtariEnv(gym.Env):
             new_order = rng.permutation(len(self.game_order)).tolist()
             self.game_order = [self.game_order[i] for i in new_order]
         
-        self.env = gym.make(self.game_order[self.current_game_idx], full_action_space=True)
-        obs, info = self.env.reset(seed=seed)
+        self.curr_game = gym.make(
+            self.game_order[self.current_game_idx],
+            full_action_space = True,
+            frameskip = 0,
+        )
+        obs, info = self.curr_game.reset(seed=seed)
+        
+        info['terminated'] = False
+        info['truncated'] = False
+        info['skipped_frames'] = []
+        info['game_name'] = self.game_order[self.current_game_idx]
+        info['reset'] = True
         
         return obs, info
+    
+    def get_current_game(self) -> gym.Env:
+        return self.curr_game
     
     def step(self, action: int) -> Tuple[np.ndarray, float, bool, bool, Dict]:
         if self.current_step >= self.steps_per_game:
             self.current_game_idx = (self.current_game_idx + 1) % len(self.game_order)
             self.current_step = 0
             
-            if self.env is not None:
-                self.env.close()
+            if self.curr_game is not None:
+                self.curr_game.close()
                 
-            self.env = gym.make(self.game_order[self.current_game_idx], full_action_space=True)
-            print(self.env.action_space)
+            self.curr_game = gym.make(
+                self.game_order[self.current_game_idx],
+                full_action_space = True,
+                frameskip = 0,
+            )
             self.seed = self.seed + 1 if self.seed is not None else None
-            obs, info = self.env.reset(seed=self.seed)
+            obs, info = self.curr_game.reset(seed=self.seed)
             reward = 0
             self.terminated = False
+            reset = True
         
         elif self.terminated:
             self.seed = self.seed + 1 if self.seed is not None else None
-            obs, info = self.env.reset(seed=self.seed)
+            obs, info = self.curr_game.reset(seed=self.seed)
             reward = 0
             self.terminated = False
-                
-        obs, reward, terminated, truncated, info = self.env.step(action)
-        self.terminated = terminated or truncated
+            reset = True
+            
+        else:
+            reset = False
         
+        skipped_frames = []
+        total_reward = 0
+        for _ in range(self.frameskip):
+            obs, reward, terminated, truncated, info = self.curr_game.step(action)
+            skipped_frames.append(obs)
+            total_reward += reward
+        
+        self.terminated = terminated or truncated
+        info['terminated'] = self.terminated
+        info['truncated'] = truncated
+        info['skipped_frames'] = skipped_frames[:-1]
+        info['game_name'] = self.game_order[self.current_game_idx]
+        info['reset'] = reset
+
         self.current_step += 1
         
-        return obs, reward, False, False, info
+        return obs, total_reward, False, False, info
